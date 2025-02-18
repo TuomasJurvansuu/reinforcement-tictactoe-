a24ae27ac420fcfb079218c538239f9b1cc8c7ea
Tuomas Jurvansuu <t9jutu01@students.oamk.fi>
2025-02-18 22:01:26 +0200

    lisätty O pelaaja

--- src/main.py
+++ src/main.py
@@ -3,10 +3,13 @@ from game import TicTacToe
 game = TicTacToe()
 game.print_board()
 
+current_player = "X"
+
 while True:
-    move = int(input("Valitse ruutu (0-8): "))
+    move = int(input(f"Pelaaja {current_player}, valitse ruutu (0-8): "))
     print(f"Siirto: {move}, Pelaaja: X")
-    if game.make_move(move, "X"):
+    if game.make_move(move, current_player):
         game.print_board()
+        current_player = "O" if current_player == "X" else "X"
     else:
         print("Virheellinen siirto, yritä uudelleen.")
