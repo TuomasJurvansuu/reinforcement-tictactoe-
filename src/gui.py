d3425ae407116512129f0da5244e6333609fa3fe
Tuomas Jurvansuu <t9jutu01@students.oamk.fi>
2025-02-25 13:58:46 +0200

    3x3 ruudukko

--- src/gui.py
+++ src/gui.py
@@ -6,13 +6,24 @@ WIDTH, HEIGHT = 500, 500
 
 WHITE = (255, 255, 255)
 
+GRID_SIZE = 3
+CELL_SIZE = WIDTH // GRID_SIZE
+LINE_WIDTH = 10
+BLACK = (0, 0, 0)
+WHITE = ( 255, 255, 255)
+
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ristinolla")
+
+def draw_grid():
+    for i in range(1, GRID_SIZE):
+        pygame.draw.line(screen, BLACK, (i * CELL_SIZE, 0), (i * CELL_SIZE, HEIGHT), LINE_WIDTH)
+        pygame.draw.line(screen, BLACK, (0, i * CELL_SIZE), (WIDTH, i * CELL_SIZE), LINE_WIDTH)
 
 running = True
 while running:
     screen.fill(WHITE)
-
+    draw_grid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
