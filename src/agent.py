ae0d23691a6626485c3272f5efd0294d73708220
Tuomas Jurvansuu <t9jutu01@students.oamk.fi>
2025-02-25 15:02:28 +0200

    Pelaajan voiton blokkaus

--- src/agent.py
+++ src/agent.py
@@ -4,15 +4,24 @@ class SmartAgent:
     def __init__(self, symbol, game):
         self.symbol = symbol
         self.game = game
+        self.opponent = "X" if symbol == "O" else "O"
 
     def choose_move(self, board):
         available_moves = [i for i, square in enumerate(board) if square == " "]
-        
+        # Voittosiirto jos mahdollista
         for move in available_moves:
             board[move] = self.symbol
             if self.game.check_winner() == self.symbol:
                 board[move] = " "
                 return move
             board[move] = " "
+        # Pelaajan blokkaus
+        for move in available_moves:
+            board[move] = self.opponent
+            if self.game.check_winner() == self.opponent:
+                board[move] = " "
+                return move
+            board[move] = " "
+
         
         return random.choice(available_moves) if available_moves else None
